2abe09bf408e02b5e0ce43d7060bb838c1cecca4
23021560-nguyenvanhoang-UET <160720222+23021560-nguyenvanhoang-UET@users.noreply.github.com>
2024-03-05 13:21:32 +0700

    Create bai2.cpp

---
+++ bai2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+bool nt(int n){
+    if(n < 2){
+        return false;
+    }
+    for(int i = 2; i <= sqrt(n); i++{
+    if(n % i == 0){
+        return false;
+
+    return true;
+
+}
+
+int main(){
+    int n;cin >> n;
+    if(nt(n)){
+        cout << "yes";
+    }
+    else{
+
+    cout << "no";
+
+}
